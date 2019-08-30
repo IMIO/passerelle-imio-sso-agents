0340957d362258fb93a4f85ab7e6c9394bcab9cd
Christophe Boulanger <christophe.boulanger@imio.be>
2019-08-30 14:34:56 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
-from setuptools.command.install import install
+from setuptools.command.install_lib import install_lib as _install_lib
 from setuptools import setup, find_packages
-import os
+
+class install_lib(_install_lib):
+    def run(self):
+        _install_lib.run(self)
 
 setup(
     name='passerelle-imio-sso-agents',
     author='Christophe Boulanger',
     author_email='christophe.boulanger@imio.be',
-    packages=find_packages(),
-    include_package_data=True,
     url='https://github.com/IMIO/passerelle-imio-sso-agents',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-    ],
-    zip_safe=False
+    packages=find_packages(),
+    install_requires=['passerelle',],
+    cmdclass={
+        'install_lib': install_lib,
+    }
 )
 
